8b533900788d57ae98f121faac5658ef16a2fba8
IamZbl <475749368@qq.com>
2018-08-19 00:08:46 +0800

    服务提供方demo

---
+++ src/main/java/com/bigdog/springcloud_eureka_client/SpringcloudEurekaClientApplication.java
@@ -0,0 +1,12 @@
+package com.bigdog.springcloud_eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaClientApplication.class, args);
+	}
+}
