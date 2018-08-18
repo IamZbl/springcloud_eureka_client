5eab6a3fbb5ffb2ee7b83808c3dfd0560c978525
IamZbl <475749368@qq.com>
2018-08-19 00:12:47 +0800

    更改目录

--- src/main/java/com/bigdog/SpringcloudEurekaClientApplication.java
+++ src/main/java/com/bigdog/SpringcloudEurekaClientApplication.java
@@ -1,4 +1,4 @@
-package com.bigdog.springcloud_eureka_client;
+package com.bigdog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
